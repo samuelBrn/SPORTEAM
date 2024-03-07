792b4fc3b0e209e0e7280e18d9acf5b8d4a69a66
Dado Biba <bibad.pro@gmail.com>
2024-03-07 14:32:11 +0100

    hover le bouton

---
+++ app/javascript/controllers/hover_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="hover"
+export default class extends Controller {
+  static targets = ["appear"]
+
+  connect() {
+    console.log("hover controller");
+  }
+
+  see() {
+    this.appearTarget.classList.remove("d-none")
+  }
+
+  hide() {
+    this.appearTarget.classList.add("d-none")
+  }
+
+}
