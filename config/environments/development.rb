1dbe1c090feccee7de16548bf84c5c67fcb2526c
Dado Biba <bibad.pro@gmail.com>
2024-03-13 13:15:28 +0100

    push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,8 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
+  # config.hosts << "4874-213-41-253-129.ngrok-free.app"
+  config.hosts << /[a-z0-9\-]+\.ngrok-free\.app/
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
