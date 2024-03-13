0685a0446fb2cdc7edc5deac313fee2b586f3c52
Dado Biba <bibad.pro@gmail.com>
2024-03-13 11:08:13 +0100

    home page merge

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,25 +4,4 @@
 //= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
-// ...
 //= link manifest.json
-{
-  "name": "Sporteam",
-  "description": "ECRIRE ICI UNE DESCRIPTION",
-  "background_color": "#242933", // Couleur à changer selon votre thème
-  "theme_color": "#242933", // Couleur à changer selon votre thème
-  "id": "/",
-  "display": "fullscreen",
-  "start_url": "/",
-  "icons": [
-    <% files = Dir.entries(Rails.root.join("app/assets/images/icons/")).select {|f| !File.directory? f} %>
-    <% files.each_with_index do |file, index| %>
-    <% match = file.match(/.+-(?<size>\d{2,3}x\d{2,3}).png/) %>
-    {
-      "src": "<%= image_path "icons/#{file}" %>",
-      "sizes": "<%= match && match[:size] %>",
-      "type": "image/png"
-    }<%= "," unless (files.size - 1) == index %>
-    <% end %>
-  ]
-}
