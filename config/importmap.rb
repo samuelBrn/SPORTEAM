dd9b44a0f5c58332341b524d9574d40474a24070
Dado Biba <bibad.pro@gmail.com>
2024-03-13 13:31:45 +0100

    fix pwa

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", to: "application.js"
+pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
@@ -8,5 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "mapbox-gl" # @3.1.2
-pin "process" # @2.0.1
+# pin "process" # @2.0.1
 pin "flatpickr" # @4.6.13
+pin "process" # @0.11.10
